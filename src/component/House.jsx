896905006a1b63f8b9cbd26fd31a7f403be6e848
Gerrard550 <privatesystem@gmail.com>
2025-05-10 11:16:09 -0700

    react

---
+++ src/component/House.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Bedroom from "./Bedroom";
+import Kitchen from "./Kitchen";
+import Parlour from "./Parlour";
+
+const House = () => {
+  return (
+    <div>
+      <Bedroom />
+      <Kitchen />
+      <Parlour />
+    </div>
+  );
+};
+
+export default House;
---
+++ src/component/Parlour.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Parlour = () => {
+  return <div>Parlour</div>;
+};
+
+export default Parlour;
